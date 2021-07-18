6119b008a03bc79bb98a5ad541a75536ad870e8b
Daniel Wust <danielwust99@gmail.com>
2021-07-18 15:02:51 -0300

    Adicionado Middleware Para Cors

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,5 +1,4 @@
-const url = "http://localhost:8080";
-// const url = "https://sistema-de-notas-back.herokuapp.com";
+const url = "https://sistema-de-notas-back.herokuapp.com";
 axios.defaults.baseURL = url;
 
 if (!localStorage.sessao) {
