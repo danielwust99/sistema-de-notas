f7aa91b8e2772722d9e065da4ed21817ab420138
Daniel Wust <danielwust99@gmail.com>
2021-07-18 23:19:52 -0300

    Trabalho Pronto.

--- src/core/presentation/App.ts
+++ src/core/presentation/App.ts
@@ -33,6 +33,14 @@ export default class App {
                 origin: "*",
             })
         );
+        this.#express.use(function (req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header(
+                "Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept"
+            );
+            next();
+        });
     }
 
     public routes() {
