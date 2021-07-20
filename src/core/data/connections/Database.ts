2efb78ca6712a98b83b8c22344232aa9a3ec5596
Daniel Wust <danielwust99@gmail.com>
2021-07-20 00:05:40 -0300

    teste

--- src/core/data/connections/Database.ts
+++ src/core/data/connections/Database.ts
@@ -3,16 +3,6 @@ import { Connection, createConnection } from "typeorm";
 export default class Database {
     private static connection: Connection;
 
-    public async getConnection() {
-        if (Database.connection === null || Database.connection === undefined) {
-            try {
-                Database.connection = await createConnection();
-            } catch (error) {
-                return { erro: error.toString().slice() };
-            }
-        }
-    }
-
     public async openConnection(): Promise<void> {
         if (Database.connection === null || Database.connection === undefined) {
             try {
