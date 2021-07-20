061dcf54bd134cdae5785f9fc46ade3ba755631b
Daniel Wust <danielwust99@gmail.com>
2021-07-20 00:07:04 -0300

    teste

--- src/features/login/middlewares/NetworkCheck.ts
+++ src/features/login/middlewares/NetworkCheck.ts
@@ -1,21 +1,21 @@
-import Database from "../../../core/data/connections/Database";
-import { Request, Response, NextFunction } from "express";
+// import Database from "../../../core/data/connections/Database";
+// import { Request, Response, NextFunction } from "express";
 
-require("dotenv").config();
-const secret: any = process.env.SECRET_HASH;
+// require("dotenv").config();
+// const secret: any = process.env.SECRET_HASH;
 
-export default async function NetworkCheck(
-    req: Request,
-    res: Response,
-    next: NextFunction
-) {
-    const testeRede = await new Database().getConnection();
+// export default async function NetworkCheck(
+//     req: Request,
+//     res: Response,
+//     next: NextFunction
+// ) {
+//     const testeRede = await new Database().getConnection();
 
-    if (testeRede) {
-        return res.json({
-            erro: testeRede.erro,
-        });
-    } else {
-        next();
-    }
-}
+//     if (testeRede) {
+//         return res.json({
+//             erro: testeRede.erro,
+//         });
+//     } else {
+//         next();
+//     }
+// }
