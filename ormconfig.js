10dcc80073bc4ace57658c702aa3ac093f60aa0b
Daniel Wust <danielwust99@gmail.com>
2021-07-20 00:04:07 -0300

    teste

--- ormconfig.js
+++ ormconfig.js
@@ -1,21 +1,23 @@
 require("dotenv").config();
 
-export const type = process.env.DB_TYPE;
-export const host = process.env.DB_HOST;
-export const port = process.env.DB_PORT;
-export const username = process.env.DB_USER;
-export const password = process.env.DB_PASSWORD;
-export const database = process.env.DB_DATABASE;
-export const synchronize = false;
-export const logging = false;
-export const entities = ["./src/core/data/database/entities/**/*"];
-export const cli = {
-  entitiesDir: "./src/core/data/database/entities",
-  migrationsDir: "./src/core/data/database/migrations"
-};
-export const migrations = ["./src/core/data/database/migrations/**/*"];
-export const extra = {
-  ssl: {
-    rejectUnauthorized: false,
+module.exports = {
+  type: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: false,
+  logging: false,
+  entities: ["./src/core/data/database/entities/**/*"],
+  cli: {
+    entitiesDir: "./src/core/data/database/entities",
+    migrationsDir: "./src/core/data/database/migrations"
+  },
+  migrations: ["./src/core/data/database/migrations/**/*"],
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 };
